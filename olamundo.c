f875a65cfe35dea77321bce430afd09a84579185
EDNA ALVES NOJIMA <sgqcontato@outlook.com>
2026-05-04 03:06:36 +0000

    Primeiro programa em C com entrada de dados

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main () {
+    printf("Ola, Mundo!\n");
+
+    int numero;
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    int resultado = numero * 100;
+
+    printf("Você digitou: %d\n", numero);
+    printf("Resultado:(x100): %d\n", resultado);
+
+    return 0;
+       
+
+
+
+
+}
